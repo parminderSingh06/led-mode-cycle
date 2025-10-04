01999f26a790a9f90c08b09a1ec1396671222116
Parminder Singh <parmindersingh1086@gmail.com>
2025-10-03 20:06:33 -0400

    Setup basic definations for i2c

---
+++ Inc/i2c.h
@@ -0,0 +1,9 @@
+#ifndef I2C_H_
+#define I2C_H_
+
+#include <stdint.h>
+#include "stm32f4xx.h"
+
+void i2c_init(void);
+
+#endif
---
+++ Src/i2c.c
@@ -0,0 +1,25 @@
+#include "i2c.h"
+
+#define GPIOBEN     (1U<<1)
+#define I2C1EN      (1U<<21)
+#define I2C_100KHZ  80
+#define SD_MODE_MAX_RISE_TIME   17
+#define CR1_PE      (1U<<0)
+#define SR2_BUSY    (1U<<1)
+#define CR1_START   (1U<<8)
+#define SR1_SB      (1U<<0)
+#define SR1_ADDR    (1U<<1)
+#define SR1_TXE     (1U<<7)
+#define CR1_ACK     (1U<<10)
+#define CR1_STOP    (1U<<9)
+#define SR1_RXNE    (1U<<6)
+#define SR1_BTF     (1U<<2)
+
+/*
+ * PB8 ---- SCL
+ * PB9 ----- SDA
+ * */
+
+void i2c_init(void){
+    
+}
