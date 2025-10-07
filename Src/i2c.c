1fa8746c6d190b0215dfa635232af5f0dea7f7ff
Parminder Singh <parmindersingh1086@gmail.com>
2025-10-06 23:00:12 -0400

    Setup i2c init function

--- Src/i2c.c
+++ Src/i2c.c
@@ -19,5 +19,44 @@
 //PB9 ----- SDA
 
 void i2c_init(void){
+    RCC->AHB1ENR |= GPIOBEN;
+
+    GPIOB->MODER &= ~(1U<<16);
+    GPIOB->MODER |= (1U<<17);
+
+    GPIOB->MODER &= ~(1U<<18);
+    GPIOB->MODER |= (1U<<19);
+
+    GPIOB->OTYPER |=(1U<<8);
+    GPIOB->OTYPER |=(1U<<9);
+
+    GPIOB->PUPDR &=~(1U<<19);
+    GPIOB->PUPDR |=(1U<<18); 
     
+    GPIOB->PUPDR &=~(1U<<17);
+    GPIOB->PUPDR |=(1U<<16);
+
+    GPIOB->AFR[1] &=~(1U<<0);
+    GPIOB->AFR[1] &=~(1U<<1);
+    GPIOB->AFR[1] |=(1U<<2);
+    GPIOB->AFR[1] &=~(1U<<3);
+
+    GPIOB->AFR[1] &=~(1U<<4);
+    GPIOB->AFR[1] &=~(1U<<5);
+    GPIOB->AFR[1] |=(1U<<6);
+    GPIOB->AFR[1] &=~(1U<<7);    
+
+    RCC->APB1ENR |= I2C1EN;
+
+    I2C1->CR1 |= (1U<<15);
+
+    I2C1->CR1 &= ~(1U<<15);
+
+    I2C1->CR2 |= (1U<<4);
+
+    I2C1->CCR = I2C_100KHZ;
+
+    I2C1->TRISE = SD_MODE_MAX_RISE_TIME;
+
+    I2C1->CR1 |= (1U<<0);
 }
